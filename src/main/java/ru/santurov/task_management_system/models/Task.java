9fcb5a6ff60c6924a3226d88622cd6256aa1bf94
Aleksandr <santurovs866@gmail.com>
2024-08-17 13:12:07 +0500

    add equals and hashcode implementation to entities

--- src/main/java/ru/santurov/task_management_system/models/Task.java
+++ src/main/java/ru/santurov/task_management_system/models/Task.java
@@ -3,8 +3,10 @@ package ru.santurov.task_management_system.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -32,4 +34,20 @@ public class Task {
 
     @ManyToMany
     private List<User> performers;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Task task = (Task) o;
+        return getId() != null && Objects.equals(getId(), task.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
