92724e56970903a0343f55ab407656055eeb7819
Aleksandr <santurovs866@gmail.com>
2024-08-20 22:51:31 +0500

    change mapstruct mapper interface to abstract class to inject dependencies directly

--- src/main/java/ru/santurov/task_management_system/services/mapper/TaskMapper.java
+++ src/main/java/ru/santurov/task_management_system/services/mapper/TaskMapper.java
@@ -1,6 +1,7 @@
 package ru.santurov.task_management_system.services.mapper;
 
 import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.santurov.task_management_system.DTO.comment.TaskCommentResponseDTO;
 import ru.santurov.task_management_system.DTO.task.*;
 import ru.santurov.task_management_system.models.Task;
@@ -11,26 +12,29 @@ import ru.santurov.task_management_system.services.UserResolver;
 import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {UserResolver.class, CommentResolver.class})
-public interface TaskMapper {
+public abstract class TaskMapper {
 
-    TaskResponseDTO toTaskResponseDTO(Task task);
+    @Autowired
+    protected UserResolver userResolver;
+
+    public abstract TaskResponseDTO toTaskResponseDTO(Task task);
 
     @Mapping(target = "comments", expression = "java(commentResolver.resolveCommentsByTaskId(task.getId()))")
-    TaskCommentResponseDTO toTaskCommentResponseDTO(Task task, @Context CommentResolver commentResolver);
+    public abstract TaskCommentResponseDTO toTaskCommentResponseDTO(Task task, @Context CommentResolver commentResolver);
 
     @Mapping(target = "author", expression = "java(userResolver.resolveCurrentUser())")
     @Mapping(target = "performers", expression = "java(taskCreateDTO.getPerformers().stream().map(userResolver::resolveByUsername).toList())")
     @Mapping(target = "status", constant = "PENDING")
-    Task toTask(TaskCreateDTO taskCreateDTO, @Context UserResolver userResolver);
+    public abstract Task toTask(TaskCreateDTO taskCreateDTO);
 
     @Mapping(target = "author", ignore = true)
     @Mapping(target = "performers", ignore = true)
     @Mapping(target = "status", expression = "java(taskUpdateDTO.getStatus() != null ? Enum.valueOf(TaskStatus.class, taskUpdateDTO.getStatus().toUpperCase()) : task.getStatus())")
     @Mapping(target = "priority", expression = "java(taskUpdateDTO.getPriority() != null ? Enum.valueOf(TaskPriority.class, taskUpdateDTO.getPriority().toUpperCase()) : task.getPriority())")
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    void updateTaskFromDto(TaskUpdateDTO taskUpdateDTO, @MappingTarget Task task, @Context UserResolver userResolver);
+    public abstract void updateTaskFromDto(TaskUpdateDTO taskUpdateDTO, @MappingTarget Task task);
 
-    default void updatePerformers(TaskUpdateDTO taskUpdateDTO, @MappingTarget Task task, UserResolver userResolver) {
+    public void updatePerformers(TaskUpdateDTO taskUpdateDTO, @MappingTarget Task task) {
         List<String> performers = taskUpdateDTO.getPerformers();
         if (performers != null && !performers.isEmpty()) {
             List<User> mappedPerformers = performers.stream()
